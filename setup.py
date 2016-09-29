c95a948754f4eb550e0fb94cba1b892b6fbe697b
Edwin Henneken <ehenneken@cfa.harvard.edu>
2016-09-29 13:03:46 -0400

    fix for install_requires

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@
 import os
 import sys
 import re
+from pip.req import parse_requirements
 
 try:
     from setuptools import setup
-
 except ImportError:
     from distutils.core import setup
 
 major, minor1, minor2, release, serial =  sys.version_info
 
 readfile_kwargs = {"encoding": "utf-8"} if major >= 3 else {}
+
+
 
 def readfile(filename):
     with open(filename, **readfile_kwargs) as fp:
@@ -26,11 +28,14 @@ contents = readfile(os.path.join(
 
 version = version_regex.findall(contents)[0]
 
+install_reqs = parse_requirements(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+reqs = [str(ir.req) for ir in install_reqs]
+
 setup(
     name = 'adsutils',
     version = version,
     long_description = readfile(os.path.join(os.path.dirname(__file__), "README.md")), 
-    install_requires = readfile(os.path.join(os.path.dirname(__file__), "requirements.txt")),
+    install_requires = reqs,
     author = 'Edwin Henneken',
     author_email = 'ehenneken@cfa.harvard.edu',
     url = 'http://github.com/adsabs/adsutils',
