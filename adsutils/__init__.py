e79b023beb01e507a86900db4eb4faa492baed7e
Edwin Henneken <ehenneken@cfa.harvard.edu>
2016-10-07 19:18:57 -0400

    typo fix

--- adsutils/__init__.py
+++ adsutils/__init__.py
@@ -2,7 +2,7 @@
 
 """ A Python tool with some general ADS utilities """
 
-__version__ = "1.0.22"
+__version__ = "1.0.23"
 
 from ads_utils import make_bibcode
 from ads_utils import resolve_references
